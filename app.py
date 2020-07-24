ce30d0be5f517c68f1b1bb4bec9cf9354bad5697
NatanCervinski <natan_cervinski@hotmail.com>
2020-07-24 10:48:07 -0300

    teste

--- app.py
+++ app.py
@@ -10,26 +10,25 @@ from resources.store_resource import Store, StoreList
 # from flask_jwt import JWT
 
 
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.secret_key = "asdasdsadasdsadasdsadasdsadasdsadasd"
+    api = Api(app)
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = "asdasdsadasdsadasdsadasdsadasdsadasd"
-api = Api(app)
+    @app.before_first_request
+    def create_table():
+        db.create_all()
 
-@app.before_first_request
-def create_table():
-    db.create_all()
+    db.init_app(app)
+    auth.init_app(app, *[authenticate, identity])
+    api.add_resource(Item, "/item/<string:name>")
+    api.add_resource(ItemList, "/items")
+    api.add_resource(UserRegister, "/register")
+    api.add_resource(Store, '/store/<string:name>')
+    api.add_resource(StoreList, '/stores')
+    return app
 
-db.init_app(app)
-auth.init_app(app, *[authenticate, identity])
-api.add_resource(Item, "/item/<string:name>")
-api.add_resource(ItemList, "/items")
-api.add_resource(UserRegister, "/register")
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
-
-if __name__ =='__main__':
-    app.run()
 
 # api.add_resource(Item, '/item/<string:name>')
